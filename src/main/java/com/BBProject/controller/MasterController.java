5aa07c17e56b600645226fe0caf205862eb03dd0
vyanky <vyankatesh9999@gmail.com>
2021-05-17 09:52:20 +0530

    code

---
+++ src/main/java/com/BBProject/controller/MasterController.java
@@ -0,0 +1,35 @@
+package com.BBProject.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author Vyanky
+ *
+ */
+@RestController
+@RequestMapping("/api/masterC")
+public class MasterController {
+
+  @GetMapping("/allemployee")
+  public String allEmployee() {
+    String retr="Connected";
+    return retr;
+  }
+
+}
---
+++ src/main/java/com/BBServer/BBServerApplication.java
@@ -0,0 +1,14 @@
+package com.BBServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+
+@SpringBootApplication
+public class BBServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BBServerApplication.class, args);
+    }
+
+}
